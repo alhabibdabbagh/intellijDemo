28cd1e9d23bdaa83455f4c491586e5d07abfc793
alhabibdabbagh <habibdabbag@gmail.com>
2020-06-16 10:41:31 +0300

    greeting with İnterface and pachage now is more fixebile

---
+++ src/com/company/greeting/Imp/Greeting.java
@@ -0,0 +1,39 @@
+package com.company.greeting.Imp;
+
+import com.company.greeting.IGreeting;
+import com.sun.deploy.panel.IProperty;
+
+public class Greeting implements IGreeting {
+    int No;
+    String Name;
+    String userName;
+    String userNam2e;
+
+    public Greeting(String userNam2e ) {
+    this.userNam2e=userNam2e;
+    }
+    public Greeting() {
+
+    }
+
+    protected void ccar() {
+        int No;
+        String Name;
+        String userName;
+        String userNam2e;
+
+//dfcdcdc
+
+        int[] sayi = new int[10];
+    }
+
+    @Override
+    public void info() {
+
+    }
+
+    @Override
+    public String bestGreeting() {
+        return null;
+    }
+}
