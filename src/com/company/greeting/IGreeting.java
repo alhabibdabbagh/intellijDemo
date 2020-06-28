0d0208f3438c8e4cb293fa8a0bb6fa0c5552aa21
alhabibdabbagh <habibdabbag@gmail.com>
2020-06-28 12:56:27 +0300

    tekrar commit atıldı

---
+++ src/com/company/greeting/IGreeting.java
@@ -0,0 +1,8 @@
+package com.company.greeting;
+
+public interface IGreeting {
+
+    void info();
+
+    String bestGreeting();
+}
--- src/com/company/starter/Main.java
+++ src/com/company/starter/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.starter;
 
 public class Main {
 
